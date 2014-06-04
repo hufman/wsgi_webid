02405c902e8b620e6569d6c8b1e726446f2e2a8c
Walter Huf <hufman@gmail.com>
2014-06-03 23:02:05 -0500

    Adds package dependencies

--- setup.py
+++ setup.py
@@ -6,10 +6,15 @@ try:
 except:
 	pass
 
+requirements = file('requirements.txt').read().split('\n')
+test_requirements = file('requirements.test.txt').read().split('\n')
+
 setup(name='wsgi_webid',
-      version='0.1.0',
+      version='0.1.1',
       description='WSGI Middleware for WebID authentication',
       author='Walter Huf',
       url='https://github.com/hufman/wsgi_webid',
-      packages=['wsgi_webid']
+      packages=['wsgi_webid'],
+      install_requires=requirements,
+      test_requires=requirements + test_requirements
      )
